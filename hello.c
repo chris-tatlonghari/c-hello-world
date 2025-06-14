492a05594723784a86216ca665cd6e1702a80f7f
chris.tatlonghari <christat98@gmail.com>
2025-06-14 10:35:35 -0400

    Explore alternate means of printing

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 int main() {
-    printf("Hello, World!\n");
+    const char * message = "Hello, World!\n";
+
+    // Using syscall to write to stdout, deprecated in macOS 10.12 :(
+    // syscall(SYS_write, 1, message, 20); 
+
+    write(1, message, 20); // POSIX-compliant and supported on macOS.
+
     return 0;
 }
